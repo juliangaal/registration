c206e7b0665e58430225935e24faf14b5757f6dc
gjulian <gjulian@uos.de>
2021-03-26 15:17:32 +0100

    local map coordinate transformation w/o bounds checking

--- include/tsdfslam/local_map.h
+++ include/tsdfslam/local_map.h
@@ -7,6 +7,7 @@
  */
 
 #include <Eigen/Dense>
+#include <tuple>
 #include <array>
 
 namespace tsdfslam
@@ -15,7 +16,12 @@ namespace tsdfslam
 template <size_t SX, size_t SY, size_t SZ, typename DTYPE = float>
 struct LocalMap
 {
-    LocalMap() : _tsdf_values{} {}
+    LocalMap() : _center_x{SX < 2 ? 1 : SX/2}, _center_y{SY < 2 ? 1 : SY/2}, _center_z{SZ < 2 ? 1 : SZ/2}
+    {
+        static_assert(SX > 2 && SX % 2 == 1, "Axis must be at least 3 && ungerade");
+        static_assert(SY > 2 && SY % 2 == 1, "Axis must be at least 3 && ungerade");
+        static_assert(SZ > 2 && SZ % 2 == 1, "Axis must be at least 3 && ungerade");
+    }
 
     ~LocalMap() = default;
 
@@ -24,16 +30,37 @@ struct LocalMap
 
     }
 
+    std::tuple<size_t, size_t, size_t> xyz2ijk(const Eigen::Vector3i& center) const
+    {
+        return {center[2], center[1], center[0]};
+    }
+
+    std::tuple<size_t, size_t, size_t> shift_to_local_map(const Eigen::Vector3i& center) const
+    {
+        return { _center_x + center[0], _center_y + center[1], _center_z + center[2] };
+    }
+
     size_t size()
     {
         return _size;
     }
 
+    const std::array<DTYPE, SX*SY*SZ> map() const
+    {
+        return _tsdf_values;
+    }
+
     constexpr static size_t _size = SX * SY * SZ;
-    std::array<DTYPE, SX * SY * SZ> _tsdf_values;
-    constexpr static size_t _center_x = SX / 2;
-    constexpr static size_t _center_y = SY / 2;
-    constexpr static size_t _center_z = SZ / 2;
+    constexpr static std::array<DTYPE, SX * SY * SZ> _tsdf_values = {};
+    size_t _center_x;
+    size_t _center_y;
+    size_t _center_z;
 };
+
+bool operator==(const Eigen::Vector3i& center, const std::tuple<size_t, size_t, size_t>& tuple)
+{
+    auto [i, j, k] = tuple;
+    return center[0] == i && center[1] == j && center[2] == k;
+}
 
 }
--- test/tsdfslam_test.cpp
+++ test/tsdfslam_test.cpp
@@ -9,6 +9,7 @@
 #include <gtest/gtest.h>
 #include <tsdfslam/local_map.h>
 #include <Eigen/Dense>
+#include <cassert>
 
 constexpr float epsilon = 0.001;
 
@@ -16,8 +17,48 @@ TEST(LocalMapTest, local_map)
 {
 	using namespace tsdfslam;
 
-	LocalMap<20, 20, 20> map;
-	ASSERT_EQ(map.size(), 20 * 20 * 20);
+	LocalMap<3, 3, 3> map;
+	ASSERT_EQ(map.size(), 3 * 3 * 3);
+
+	for (const auto& value: map.map())
+    {
+	    ASSERT_EQ(value, 0.0f);
+    }
+
+    {
+        auto[x, y, z] = map.shift_to_local_map({0, 0, 0});
+        ASSERT_EQ(x, 1);
+        ASSERT_EQ(y, 1);
+        ASSERT_EQ(z, 1);
+    }
+
+    {
+        auto[x, y, z] = map.shift_to_local_map({-1, -1, 0});
+        ASSERT_EQ(x, 0);
+        ASSERT_EQ(y, 0);
+        ASSERT_EQ(z, 1);
+    }
+
+    {
+        auto[x, y, z] = map.shift_to_local_map({-1, -1, -1});
+        ASSERT_EQ(x, 0);
+        ASSERT_EQ(y, 0);
+        ASSERT_EQ(z, 0);
+    }
+
+    {
+        auto[x, y, z] = map.shift_to_local_map({1, 1, 1});
+        ASSERT_EQ(x, 2);
+        ASSERT_EQ(y, 2);
+        ASSERT_EQ(z, 2);
+    }
+
+    {
+        auto[x, y, z] = map.shift_to_local_map({1, 1, -1});
+        ASSERT_EQ(x, 2);
+        ASSERT_EQ(y, 2);
+        ASSERT_EQ(z, 0);
+    }
 }
 
 int main(int argc, char **argv)
