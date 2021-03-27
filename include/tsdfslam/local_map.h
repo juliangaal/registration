9526e31037614c3d98a5b6c9e525534d8ed1efc3
juliangaal <gjulian@uos.de>
2021-03-27 13:08:29 +0100

    add bounds checks for local map, test 3d21d

--- include/tsdfslam/local_map.h
+++ include/tsdfslam/local_map.h
@@ -14,8 +14,9 @@ namespace tsdfslam
 {
 
 template <size_t SX, size_t SY, size_t SZ, typename DTYPE = float>
-struct LocalMap
+class LocalMap
 {
+public:
     LocalMap() : _center_x{SX < 2 ? 1 : SX/2}, _center_y{SY < 2 ? 1 : SY/2}, _center_z{SZ < 2 ? 1 : SZ/2}
     {
         static_assert(SX > 2 && SX % 2 == 1, "Axis must be at least 3 && ungerade");
@@ -27,18 +28,50 @@ struct LocalMap
 
     void update(const Eigen::Vector3i& center, DTYPE value)
     {
-
+		auto index = _3d21d(center);
+		if (index == -1)
+		{
+			return;
+		}
+		
+		if (_tsdf_values.at(index) > value && _tsdf_values.at(index) != 0)
+		{
+			_tsdf_values[index] = value;
+		}
     }
-
-    std::tuple<size_t, size_t, size_t> xyz2ijk(const Eigen::Vector3i& center) const
+	
+    template <typename T>
+    Eigen::Vector3i xyz2ijk(const T& center) const
     {
         return {center[2], center[1], center[0]};
     }
 
-    std::tuple<size_t, size_t, size_t> shift_to_local_map(const Eigen::Vector3i& center) const
+    Eigen::Vector3i shift_to_local_map(const Eigen::Vector3i& center) const
     {
         return { _center_x + center[0], _center_y + center[1], _center_z + center[2] };
     }
+    
+	int _3d21d(const Eigen::Vector3i& center)
+	{
+    	// bounds check
+    	if (center[0] > SX / 2 || center[1] > SY / 2 || center[2] > SZ / 2)
+		{
+    		return -1;
+		}
+    	
+    	// shift into local coordinate system
+		auto center_shifted = shift_to_local_map(center);
+    	
+    	// convert xyz representation into ijk
+    	// x -> depth, y -> width, z -> height
+		auto center_axis_shifted = xyz2ijk(center_shifted);
+		
+		int i = center_axis_shifted[0];
+		int j = center_axis_shifted[1];
+		int k = center_axis_shifted[2];
+		
+		return i * SY * SZ + j * SZ + k;
+	}
 
     size_t size()
     {
@@ -49,12 +82,13 @@ struct LocalMap
     {
         return _tsdf_values;
     }
-
+    
+private:
     constexpr static size_t _size = SX * SY * SZ;
     constexpr static std::array<DTYPE, SX * SY * SZ> _tsdf_values = {};
-    size_t _center_x;
-    size_t _center_y;
-    size_t _center_z;
+    int _center_x;
+    int _center_y;
+    int _center_z;
 };
 
 bool operator==(const Eigen::Vector3i& center, const std::tuple<size_t, size_t, size_t>& tuple)
--- test/tsdfslam_test.cpp
+++ test/tsdfslam_test.cpp
@@ -16,9 +16,12 @@ constexpr float epsilon = 0.001;
 TEST(LocalMapTest, local_map)
 {
 	using namespace tsdfslam;
-
-	LocalMap<3, 3, 3> map;
-	ASSERT_EQ(map.size(), 3 * 3 * 3);
+	constexpr static size_t SX = 3;
+	constexpr static size_t SY = 3;
+	constexpr static size_t SZ = 3;
+	LocalMap<SX, SY, SZ> map;
+	
+	ASSERT_EQ(map.size(), SX * SY * SZ);
 
 	for (const auto& value: map.map())
     {
@@ -26,39 +29,108 @@ TEST(LocalMapTest, local_map)
     }
 
     {
-        auto[x, y, z] = map.shift_to_local_map({0, 0, 0});
-        ASSERT_EQ(x, 1);
-        ASSERT_EQ(y, 1);
-        ASSERT_EQ(z, 1);
+        auto center_shifted =  map.shift_to_local_map({0, 0, 0});
+        ASSERT_EQ(center_shifted[0], 1);
+        ASSERT_EQ(center_shifted[1], 1);
+        ASSERT_EQ(center_shifted[2], 1);
     }
 
     {
-        auto[x, y, z] = map.shift_to_local_map({-1, -1, 0});
-        ASSERT_EQ(x, 0);
-        ASSERT_EQ(y, 0);
-        ASSERT_EQ(z, 1);
+        auto center_shifted =  map.shift_to_local_map({-1, -1, 0});
+        ASSERT_EQ(center_shifted[0], 0);
+        ASSERT_EQ(center_shifted[1], 0);
+        ASSERT_EQ(center_shifted[2], 1);
     }
 
     {
-        auto[x, y, z] = map.shift_to_local_map({-1, -1, -1});
-        ASSERT_EQ(x, 0);
-        ASSERT_EQ(y, 0);
-        ASSERT_EQ(z, 0);
+        auto center_shifted =  map.shift_to_local_map({-1, -1, -1});
+        ASSERT_EQ(center_shifted[0], 0);
+        ASSERT_EQ(center_shifted[1], 0);
+        ASSERT_EQ(center_shifted[2], 0);
     }
 
     {
-        auto[x, y, z] = map.shift_to_local_map({1, 1, 1});
-        ASSERT_EQ(x, 2);
-        ASSERT_EQ(y, 2);
-        ASSERT_EQ(z, 2);
+        auto center_shifted =  map.shift_to_local_map({1, 1, 1});
+        ASSERT_EQ(center_shifted[0], 2);
+        ASSERT_EQ(center_shifted[1], 2);
+        ASSERT_EQ(center_shifted[2], 2);
     }
 
     {
-        auto[x, y, z] = map.shift_to_local_map({1, 1, -1});
-        ASSERT_EQ(x, 2);
-        ASSERT_EQ(y, 2);
-        ASSERT_EQ(z, 0);
+        auto center_shifted =  map.shift_to_local_map({1, 1, -1});
+        ASSERT_EQ(center_shifted[0], 2);
+        ASSERT_EQ(center_shifted[1], 2);
+        ASSERT_EQ(center_shifted[2], 0);
     }
+	
+	// center in map should be center in representation
+	{
+		int tsdf_index = map._3d21d({0, 0, 0});
+		ASSERT_EQ(map.size() / 2, tsdf_index);
+	}
+	
+	// Move along x axis --> increment 1
+	{
+		int start_index = map.size() / 2;
+		int index_increment = 1;
+		
+		
+		for (int i = 0; i < SX; ++i)
+		{
+			int tsdf_index = map._3d21d({i, 0, 0});
+			
+			if (i <= SX / 2)
+			{
+				ASSERT_EQ(tsdf_index, start_index + i * index_increment);
+			}
+			else
+			{
+				ASSERT_EQ(tsdf_index, -1);
+			}
+		}
+	}
+	
+	// Move along y axis --> increment SY
+	{
+		int start_index = map.size() / 2 + SY;
+		int index_increment = SY;
+
+
+		for (int i = 1; i < SX; ++i)
+		{
+			int tsdf_index = map._3d21d({0, i, 0});
+
+			if (i <= SY / 2)
+			{
+				ASSERT_EQ(tsdf_index, start_index + (i-1) * index_increment);
+			}
+			else
+			{
+				ASSERT_EQ(tsdf_index, -1);
+			}
+		}
+	}
+	
+	// Move along z axis --> increment SY * SZ
+	{
+		int start_index = map.size() / 2 + SY*SZ;
+		int index_increment = SY*SZ;
+		
+		
+		for (int i = 1; i < SZ; ++i)
+		{
+			int tsdf_index = map._3d21d({0, 0, i});
+			
+			if (i <= SY / 2)
+			{
+				ASSERT_EQ(tsdf_index, start_index + (i-1) * index_increment);
+			}
+			else
+			{
+				ASSERT_EQ(tsdf_index, -1);
+			}
+		}
+	}
 }
 
 int main(int argc, char **argv)
